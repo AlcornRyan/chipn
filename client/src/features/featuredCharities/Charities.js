cda12ed58b3a1a0926829e07c386e2ef0f2d2316
Nupur Bhatt <Nupur.Bhatt@dal.ca>
2020-11-22 17:01:21 -0400

    Featured Charities Setup

---
+++ client/src/features/featuredCharities/Charities.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Card } from '@material-ui/core';
+
+
+class Charities extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+               <Card>
+                   Card
+               </Card>
+            </div>
+         );
+    }
+}
+
+export default Charities;
