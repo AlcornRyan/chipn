a1caaa903b4c9cdb36a8db721f1254ecab21757c
Ryan Alcorn <ry737126@dal.ca>
2020-11-30 16:38:54 -0400

    New Branch: Created new branch for discoverCause portion of the Homepage

--- client/src/pages/homepage/components/DiscoverCause.js
+++ client/src/pages/homepage/components/DiscoverCause.js
@@ -1,9 +1,10 @@
 import './DiscoverCause.css';
+import Grid from '@material-ui/core/Grid';
 
 export default function PrintDiscoverCause() {
   return (
     <div className="discoverCauseContainer">
-      Discover a cause
+      Discover a cause portion
     </div>
   );
 }
