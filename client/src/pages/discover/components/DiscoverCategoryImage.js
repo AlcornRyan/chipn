cbbb22be70f5854e91e40178cb179088e700b9a7
Ryan Alcorn <ry737126@dal.ca>
2020-12-02 23:04:55 -0400

    Finished DiscoverCategoryImage component

--- client/src/pages/discover/components/DiscoverCategoryImage.js
+++ client/src/pages/discover/components/DiscoverCategoryImage.js
@@ -1,12 +1,15 @@
 import './DiscoverCategoryImage.css';
 import Grid from '@material-ui/core/Grid';
-import forestBG from '../../../images/brighter-forest.jpg';
+import forestBG from '../../../images/Brighter-forest.jpg';
 
 export default function printDiscoverCategoryImage() {
   return (
     <Grid container className="dis_CategoryImageContainer">
       <Grid item md={12} className="dis_CategoryImage">
         <img src={forestBG} alt="Elephant walking in desert" className="dis_CategoryBackgroundImage" />
+      </Grid>
+      <Grid item md={12} className="dis_CategoryImageTitle">
+        <h1>Environmental</h1>
       </Grid>
     </Grid>
   );
