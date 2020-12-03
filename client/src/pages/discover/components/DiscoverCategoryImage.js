0fec3cfd267189d799c9eda4c386ea878fa7301b
Ryan Alcorn <ry737126@dal.ca>
2020-12-02 22:49:40 -0400

    Added Grid tags and styling to DiscoverCategoryImage component as well as environment background image

--- client/src/pages/discover/components/DiscoverCategoryImage.js
+++ client/src/pages/discover/components/DiscoverCategoryImage.js
@@ -1,10 +1,14 @@
 import './DiscoverCategoryImage.css';
+import Grid from '@material-ui/core/Grid';
+import forestBG from '../../../images/brighter-forest.jpg';
 
 export default function printDiscoverCategoryImage() {
   return (
-    <div className="discoverCategoryImage">
-      Discover Category Image
-    </div>
+    <Grid container className="dis_CategoryImageContainer">
+      <Grid item md={12} className="dis_CategoryImage">
+        <img src={forestBG} alt="Elephant walking in desert" className="dis_CategoryBackgroundImage" />
+      </Grid>
+    </Grid>
   );
 }
   
