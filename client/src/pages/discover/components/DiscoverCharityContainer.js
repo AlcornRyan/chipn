7f6367d48584707cb51ae0a2bf1280b38abd5127
Ryan Alcorn <ry737126@dal.ca>
2020-12-02 23:22:41 -0400

    Created charity card shell to display on discover page

--- client/src/pages/discover/components/DiscoverCharityContainer.js
+++ client/src/pages/discover/components/DiscoverCharityContainer.js
@@ -1,10 +1,58 @@
 import './DiscoverCharityContainer.css';
+import Grid from '@material-ui/core/Grid';
 
 export default function printDiscoverCharityContainer() {
   return (
-    <div className="discoverCharityContainer">
-      Discover Charity Container
-    </div>
+    <Grid container justify="center" className="DiscoverCharityContainer" spacing={3}>
+      
+      <Grid item md={4} className="dis_CharityCardContainer">
+        <Grid container>
+          <Grid item md={12} className="dis_CharityCard">
+            1
+          </Grid>
+        </Grid>
+      </Grid>
+      
+      <Grid item md={4} className="">
+        <Grid container>
+          <Grid item md={12} className="dis_CharityCard">
+            2
+          </Grid>
+        </Grid>
+      </Grid>
+      
+      <Grid item md={4} className="">
+        <Grid container>
+          <Grid item md={12} className="dis_CharityCard">
+            3
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid item md={4} className="dis_CharityCardContainer">
+        <Grid container>
+          <Grid item md={12} className="dis_CharityCard">
+            4
+          </Grid>
+        </Grid>
+      </Grid>
+      
+      <Grid item md={4} className="">
+        <Grid container>
+          <Grid item md={12} className="dis_CharityCard">
+            5
+          </Grid>
+        </Grid>
+      </Grid>
+      
+      <Grid item md={4} className="">
+        <Grid container>
+          <Grid item md={12} className="dis_CharityCard">
+            6
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
   
