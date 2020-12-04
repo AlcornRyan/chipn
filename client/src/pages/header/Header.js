e679acd2b63a26af64b6d61bc9518ce48f1b4327
Nupur Bhatt <Nupur.Bhatt@dal.ca>
2020-12-04 16:09:28 -0400

    Fixed header alignment and changed design

--- client/src/pages/header/Header.js
+++ client/src/pages/header/Header.js
@@ -7,11 +7,7 @@ import Slide from '@material-ui/core/Slide';
 import PropTypes from 'prop-types';
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Search from '@material-ui/icons/SearchOutlined';
-import { Toolbar, Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
-import IconButton from '@material-ui/core/IconButton';
+
 
 function HideOnScroll(props) {
   const { children, window } = props;
@@ -29,87 +25,55 @@ HideOnScroll.propTypes = {
   window: PropTypes.func,
 }
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  item: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-  },
-}));
-
 export default function PrintHeader(props) {
 
-  const classes = useStyles();
-
   return (
-   <div className={classes.root}>
-      <Grid container 
-      direction="row" 
-      style={{backgroundColor:"white", marginBottom:"4em", 
-      flexGrow:"1", justify:"center", alignItems:"flex-start"}}>
+    <Grid container >
         {/* Header Container */}
-
         <React.Fragment>
         <CssBaseline />
       <HideOnScroll {...props}>
-        <AppBar style={{backgroundImage:'url("/images/wtiger.png")', backgroundSize: "cover", color:"transparent"}} >
-          <Toolbar>
+        <AppBar className="appbar">
 
+        <Grid container direction="row" className="gridcontain">
 
-        <Grid item className="chipnLogo">
+        <Grid item className="items">
             {/* Chipn Logo */}
-            <NavLink to="/">
-              <img src="/chipnLogoW3.svg" alt="Chipn Logo"  style={{width:"95px", height:"35px", 
-              objectFit:"cover"}}/>
+            
+            <NavLink to="/" className="logoContainer">
+              <img src="/chipnLogo.svg" alt="Chipn Logo" className="logochipn"/>
             </NavLink>
         </Grid>
 
-
-          <Grid item  >
+        <Grid item className="items">
            {/* Discover Link */}
-           <NavLink to="/discover" className="discover" style={{border: "1px solid transparent",
-              borderRadius: "10px", padding:"10px"}}>discover</NavLink>
-          </Grid>
+           <NavLink to="/discover" className="discover" >discover</NavLink>
+        </Grid>
 
-
-          <Grid item>
-          <Search style={{color:"white", justify:"center"}}/>
-          </Grid>     
-
-          <Grid item >
+        <Grid item className="items">
           {/* Search bar */}
-            <input type="text" id="searchBar" className="search"
+            <input type="text" className="search" placeholder="Search Charities"
             style={{backgroundColor:"transparent",
-            border: "1px solid white",
-            placeholder:"search charities",
-            padding:"10px",
-            borderRadius:"10px", 
-            color:"textPrimary"}} />
-            </Grid>
-       
+            border: "1px solid white"}} />
+        </Grid>
 
-       <Grid item spacing={2}>
+        <Grid item className="items">
             {/* Sign in Link */}
-            <NavLink to="/signin" className="login" style={{border: "1.4px solid white",
-              borderRadius: "10px", padding:"10px"}}>
-           login
-           
+            <NavLink to="/signin" className="login">
+                login
+            </NavLink>
+        </Grid>
+
+        <Grid item className="items">
+              {/* Sign up Link */}
+              <NavLink to="/signup" className="register">
+                register
             </NavLink>
             </Grid>
-
-
-            <Grid item spacing={2}>
-              {/* Sign up Link */}
-              <NavLink to="/signup" className="signup">sign up</NavLink>
-            </Grid>
-
-        </Toolbar>
+        </Grid>
       </AppBar>
       </HideOnScroll>
       </React.Fragment>
       </Grid>
-      </div>
   );
 }
