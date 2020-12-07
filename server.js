ff567f89cd405d39a72f1190c3b99fdb98ebf318
Nupur Bhatt <Nupur.Bhatt@dal.ca>
2020-12-07 11:28:08 -0400

    Endpoints completed

--- server.js
+++ server.js
@@ -20,12 +20,14 @@ db.connect((err) =>{
         throw err;
 });
 
+
 //GET REQUESTS 
 
 //endpoint to check if '......./' works --- SAMPLE TEST
 app.get('/', (req, res) => {
     res.send('Welcome to the Chipn backend server!');
 })
+
 
 //endpoint to Obtain ALL types of CATEGORIES for the Discover page.
 app.get('/categories', (req, res) =>{
@@ -36,6 +38,7 @@ app.get('/categories', (req, res) =>{
     });
 })
 
+
 //endpoint to obtain two charities for Featured Charities
 app.get('/featured', (req, res) =>{
     const db_query= 'select u.first_name, u.city, u.state, u.country, c.description, c.views from user u, charity_role c where c.charity_id = u.user_id limit 2';
@@ -45,10 +48,47 @@ app.get('/featured', (req, res) =>{
     });
 })
 
+
 //endpoint to retrieve all charities for discover page.
 app.get('/charities', (req, res) =>{
-    const db_query= 'u.first_name, u.city, u.state, u.country, c.description from user u, charity_role c where c.charity_id = u.user_id order by u.first_name asc';
+    const db_query= 'select u.first_name, u.city, u.state, u.country, c.description from user u, charity_role c where c.charity_id = u.user_id order by u.first_name';
     db.query(db_query, (err, result) =>{
+        console.log(result);
+        res.send(result);
+    });
+})
+
+//to collect all charity info based on userid -   /:user_id
+app.get('/:email_id', (req, res) =>{
+    const db_query= 'select * from user where email_id=?';
+    const values = [req.params.email_id];
+    db.query(db_query, values, (err, result) =>{
+        console.log(result);
+        res.send(result);
+    });
+})
+
+//endpoint to retrieve all donors for discover page.
+app.get('/charities/:charity_id', (req, res) =>{
+    const db_query= 'select count(distinct user_id) as donors from donor_role where charity_id=?';
+    const values = [req.params.charity_id];
+    db.query(db_query, values, (err, result) =>{
+
+        if(err)
+        {
+            res.send('Unfortunately, there are no donors yet.');
+        }
+        console.log(result);
+        res.send(result);
+    });
+})
+
+
+//endpoint to retrieve all charities for discover page.
+app.get('/charities/donation/:charity_id', (req, res) =>{
+    const db_query= 'select sum(donation_amount) as total_donation_amount from donor_role where charity_id=?';
+    const values = [req.params.charity_id];
+    db.query(db_query, values, (err, result) =>{
         console.log(result);
         res.send(result);
     });
@@ -59,83 +99,58 @@ app.get('/charities', (req, res) =>{
 const jsonParser = bodyParser.json();
 
 
-
-
 //endpoint to ADD NEW USER
 app.post('/signup', jsonParser, (req, res) =>{
-
-//check if user exists
-let db_query = 'select * from user where email_id = ?';
-let values = [req.body.email_id];
-var result = db.query(db_query,values, (err, result_outer) => {
-
-    if(err)
-        throw err;
-    if(result_outer==""){
-
-        //add user if they dont exist when result set is null
-        const db_query_inner = 'insert into user(email_id, first_name, address_line_1, address_line_2, city, state, country) values (?,?,?,?,?,?,?)';
-        let values_inner = [req.body.email_id, req.body.first_name, req.body.address_line_1, req.body.address_line_2, req.body.city, req.body.state, req.body.country];
-        var result_inner = db.query(db_query_inner, values_inner, (error, res) =>{
-
-            if(err)
-                throw err;
-            res.send('SUCCESS!');
+        const db_query_inner = 'insert into user(email_id, first_name, address_line_1, address_line_2, city, state, country, password) values (?,?,?,?,?,?,?,?)';
+        let values_inner = [req.body.email_id, req.body.first_name, req.body.address_line_1, req.body.address_line_2, req.body.city, req.body.state, req.body.country, req.body.password];
+        var result_inner = db.query(db_query_inner, values_inner, (error, results) =>{
+            
+            if(error){
+                res.send('User exists already!!!!!!!');
+            }
+            else{
+                res.send('OK!');
+            }           
         })
-    }
-});
 })
-
 
 
 //endpoint for USER LOGIN
+app.post('/signin', jsonParser, (req, res) =>{
 
-app.post('/signin', (req, res) =>{
-
-let db_query = 'select * from user where email_id = ?';
+let db_query = 'select password from user where email_id = ?';
 let values = [req.body.email_id];
-var result = db.query(db_query,values, (err, result_outer) => {
 
-    if(err)
-        throw err;
-    if(result_outer==""){
+var result = db.query(db_query, values, (err, result_outer) => {
 
-        //add user if they dont exist when result set is null
-        const db_query_inner = 'select password from user where email_id = ?;';
-        let values_inner = [req.body.email_id];
-
-        var result_inner = db.query(db_query_inner, values_inner, (error, res) =>{
-            if(err)
-                throw err;
-           if(res == req.body.password){
-               res.send('SUCCESS');
-           }
-        })
+    if(err){
+        res.send('Sign up for an account if you have not done so!');
     }
-});
+    if(req.body.password === result_outer[0].password){
+        res.send('SUCCESSFULLY LOGGED IN!');
+    }
+    else{
+        res.send('INCORRECT PASSWORD!');
+    }
+})
 })
 
-//endpoint to retrieve user data for 
-//session management (after user logs in)
-// app.get('/:user_id', (req, res) =>{
 
-//     //check if user is a charity or some donor.
+//endpoint to allow users to make donation. 
+app.post('/donate', jsonParser, (req, res) =>{
+
+    let db_query = 'insert into donor_role(email_id, charity_id, donation_amount, date_of_donation) values (?, ?, ?, ?)';
+    let values = [req.body.email_id, req.body.charity_id, req.body.donation_amount, req.body.date_of_donation];
+    var result = db.query(db_query, values, (err, result_outer) => {
     
-
-//     //if result is null, user is not a charity. 
-//     //query donor_role and user table; 
-
-
-//     //if result is not null, use that result from charity_role
-//     //and query user table as well for general info. 
-
-//     const db_query= '';
-//     db.query(db_query, (err, result) =>{
-//     console.log(result);
-//     res.send('Login Page');
-// })
-// })
-
-app.listen(3001, () => {
-    console.log("Backend server running on port 3001.");
+        if(err){
+            res.send('Error making a donation, make sure the values you enter are correct');
+        }
+        else{
+            res.send('DONATION SUCCESSFULLY MADE!');
+        }
+    })
 })
+
+//start the app by listening on default port
+app.listen(process.env.PORT || 8080);
