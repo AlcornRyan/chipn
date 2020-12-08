b316156ad46995fd243b39a880fbd7b06abb5cbc
Ryan Alcorn <ry737126@dal.ca>
2020-12-07 21:05:16 -0400

    testing new header

--- src/pages/header/Header.js
+++ src/pages/header/Header.js
@@ -2,41 +2,47 @@ import './Header.css';
 import Grid from '@material-ui/core/Grid';
 import ChipnLogo from '../../logos/chipn-logo.svg';
 import { NavLink } from 'react-router-dom';
+import MenuItem from '@material-ui/core/MenuItem';
 
 export default function PrintHeader() {
   return (
-      <Grid container className="headerContainer">
-        {/* Header Container */}
-        <Grid item md={12}>
-          
+  <Grid container className="headerContainer">
+    <Grid item xs={12}>
+      {/* Header Container */}
+      <Grid container>
+            
+        {/* Left side */}
+        <Grid item sm={4}>
+              
           {/* Chipn Logo */}
-          <div className="logoContainer">
+          <Grid className="logoContainer">
             <NavLink to="/">
               <img src={ChipnLogo} alt="Chipn Logo" className="chipnLogo" />
             </NavLink>
-          </div>
+          </Grid>
 
           {/* Discover Link */}
           <NavLink to="/discover" className="">Discover</NavLink>
-          
-        
-        {/* Right side of Header */}
-        <div id="rightSideContainer">
-          
-          {/* Search bar */}
-          <div id="searchContainer">
-            <input type="text" id="searchBar" placeholder="Search charities" />
-          </div>
-          
-          {/* Sign in Link */}
-          <NavLink to="/signin" className="signInLink rightLinks">Sign in</NavLink>
-
-          {/* Sign up Link */}
-          <NavLink to="/signup" className="rightLinks">Sign up</NavLink>
-
-        </div>
 
         </Grid>
+            
+        {/* Right side */}
+        <Grid item sm={8} className="rightSideContainer">
+          <Grid container justify="flex-end">
+            {/* Search bar */}
+            <Grid  id="searchContainer">
+              <input type="text" id="searchBar" placeholder="Search charities" />
+            </Grid>
+                
+            {/* Sign in Link */}
+            <NavLink to="/signin" className="signInLink rightLinks">Sign in</NavLink>
+
+            {/* Sign up Link */}
+            <NavLink to="/signup" className="rightLinks">Sign up</NavLink>
+          </Grid>
+        </Grid>
       </Grid>
+    </Grid>
+  </Grid>
   );
 }
---
+++ src/pages/header/HeaderTwo.js
@@ -0,0 +1,15 @@
+import './HeaderTwocss';
+import Grid from '@material-ui/core/Grid';
+import ChipnLogo from '../../logos/chipn-logo.svg';
+import { NavLink } from 'react-router-dom';
+import MenuItem from '@material-ui/core/MenuItem';
+
+export default function PrintHeader() {
+  return (
+    <Grid container>
+      <Grid item md={12}>
+        
+      </Grid>
+    </Grid>
+  );
+}
